8dd35b948b297692ff9cb4233d5656a112fcacb6
Alex P. Andrade <g.alex.andrade@gmail.com>
2016-12-17 15:18:13 -0200

    Commit

--- OpenSchool/src/dao/UserDAO.java
+++ OpenSchool/src/dao/UserDAO.java
@@ -3,10 +3,6 @@ package dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -15,7 +11,7 @@ import model.*;
 
 public class UserDAO {
 	private static UserDAO instance;
-	protected Session session;
+	private SessionFactory sessionFactory;
 	
 	public static UserDAO getInstance(){
 		if (instance == null){
@@ -26,21 +22,67 @@ public class UserDAO {
 	}
 
 	private UserDAO() {
-		session = getEntityManager();
+		sessionFactory = getEntityManager();
 	}
 
-	private Session getEntityManager() {
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		return session;
+	private SessionFactory getEntityManager() {
+		return new Configuration().configure().buildSessionFactory();
+	}
+	
+	public User getUser(final int id) {
+		Session session = sessionFactory.openSession();;
+		session.beginTransaction();	
+		
+		User user = (User) session.get(User.class, id);
+		
+		session.close();
+		return user;
+	}
+	
+	public void deleteUser(final int id) {
+		User user = getUser(id);
+		
+		deleteUser(user);
+	}
+	
+	public void deleteUser(final User user) {
+		Session session = sessionFactory.openSession();;
+		session.beginTransaction();	
+		
+		session.delete(user);
+		session.getTransaction().commit();
+		
+		session.close();
+	}
+	
+	public void addUser(final User user) {
+		Session session = sessionFactory.openSession();;
+		session.beginTransaction();	
+		
+		session.persist(user);
+		session.getTransaction().commit();
+		
+		session.close();
+	}
+	
+	public void updateUser(final User user) {
+		Session session = sessionFactory.openSession();;
+		session.beginTransaction();	
+		
+		session.merge(user);
+		session.getTransaction().commit();
+		
+		session.close();
 	}
 	
 	public List<User> listUsers(){
-		List<User> users = new ArrayList<>();				
-		User user = (User) session.get(User.class, 1);		
-		users.add(user);
+		List<User> users = new ArrayList<>();		
+		
+		Session session = sessionFactory.openSession();;
+		session.beginTransaction();		
+		
+		/*User user = (User) session.get(User.class, 1);*/
+		users = session.createQuery("FROM " + User.class.getName()).list();
 		
 		session.close();
 		
--- OpenSchool/src/rest/UserService.java
+++ OpenSchool/src/rest/UserService.java
@@ -2,8 +2,13 @@ package rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,6 +18,21 @@ import model.User;
 @Path("/user")
 public class UserService {
 	private static final String CHARSET_UTF8 = ";charset=utf-8";
+	
+	@GET
+	@Path("/{id}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUser(@PathParam("id") int idUser){
+		
+		User user = null;
+		try{
+			user = UserDAO.getInstance().getUser(idUser);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return user;
+	}
 	
 	@GET
 	@Path("/list")
@@ -26,5 +46,58 @@ public class UserService {
 			e.printStackTrace();
 		}
 		return list;
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUser(@PathParam("id") int idUser){
+		String result;
+		
+		try{
+			UserDAO.getInstance().deleteUser(idUser);
+			result = "OK";
+		}catch(Exception e){
+			e.printStackTrace();
+			result = "ERROR";
+		}
+		return result;
+	}
+	
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addUser(User user){
+		String result;
+		
+		try{
+			UserDAO.getInstance().addUser(user);
+			result = "OK";
+		}catch(Exception e){
+			e.printStackTrace();
+			result = "ERROR";
+		}
+		
+		return result;
+	}
+	
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUser(User user){
+		String result;
+		
+		try{
+			UserDAO.getInstance().updateUser(user);
+			result = "OK";
+		}catch(Exception e){
+			e.printStackTrace();
+			result = "ERROR";
+		}
+		
+		return result;
 	}
 }
