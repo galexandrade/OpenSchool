f0e66e4ae259afa459939e6b8a58d051fec28745
Alex P. Andrade <g.alex.andrade@gmail.com>
2017-01-26 21:23:07 -0200

    Commit

--- OpenSchool/WebContent/js/openschool.management.students.js
+++ OpenSchool/WebContent/js/openschool.management.students.js
@@ -1,43 +1,48 @@
 $(function(){    
 	var handleDataTableButtons = function() {
-        if ($("#students_datatable").length) {
-          $("#students_datatable").DataTable({
-            dom: "Bfrtip",
-            buttons: [
-              {
-                extend: "copy",
-                className: "btn-sm"
-              },
-              {
-                extend: "csv",
-                className: "btn-sm"
-              },
-              {
-                extend: "excel",
-                className: "btn-sm"
-              },
-              {
-                extend: "pdfHtml5",
-                className: "btn-sm"
-              },
-              {
-                extend: "print",
-                className: "btn-sm"
-              },
-            ],
-            responsive: true
-          });
-        }
-      };
-      
-      TableManageButtons = function() {
-        "use strict";
-        return {
-          init: function() {
-            handleDataTableButtons();
-          }
-        };
-      }();
-
-      TableManageButtons.init();
+	    if ($("#students_datatable").length) {
+	      $("#students_datatable").DataTable({
+	        dom: "Bfrtip",
+	        buttons: [
+	          {
+	            extend: "copy",
+	            className: "btn-sm"
+	          },
+	          {
+	            extend: "csv",
+	            className: "btn-sm"
+	          },
+	          {
+	            extend: "excel",
+	            className: "btn-sm"
+	          },
+	          {
+	            extend: "pdfHtml5",
+	            className: "btn-sm"
+	          },
+	          {
+	            extend: "print",
+	            className: "btn-sm"
+	          },
+	        ],
+	        responsive: true
+	      });
+	    }
+	  };
+	  
+	  TableManageButtons = function() {
+	    "use strict";
+	    return {
+	      init: function() {
+	        handleDataTableButtons();
+	      }
+	    };
+	  }();
+	
+	  TableManageButtons.init();
+	  
+	  $("#students_datatable tbody tr td .btn-group button").focusout(function(){
+		  $(this).attr({"aria-expanded": "false"});
+		  $(this).parent().removeClass("open");
+	  });
 });
